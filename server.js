50b50bd7ca6954a6aaf2ddf5b02703de3010bb40
BeiningSAN <sanbeining@gmail.com>
2025-11-14 17:26:14 +0100

    Use PORT from environment for Render

--- server.js
+++ server.js
@@ -177,6 +177,9 @@ io.on("connection", (socket) => {
 });
 
 // ---- START SERVER ----
-server.listen(3000, () => {
-  console.log("Backend server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Backend server running on port ${PORT}`);
 });
+
